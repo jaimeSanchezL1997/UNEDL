b472ae0ba36929214a0f90de7e45ea7629c0c89b
jaimeSanchezL1997 <48300839+jaimeSanchezL1997@users.noreply.github.com>
2019-05-28 16:41:22 -0500

    Add files via upload

---
+++ Docs/excepcion/excepcion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepcion
+{
+    class Program
+    {
+        static Exception miExcepcion = new Exception("no te hagas pato no se puede dividir entre 0");
+
+        static void Main(string[] args)
+        {
+            float num1,num2,num3;
+            try
+            {
+                Console.WriteLine("dame numeros a dividir");
+                Console.WriteLine("dame numero 1");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("dame numero 2");
+                num2 = int.Parse(Console.ReadLine());
+                MiMetodo();
+     
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Algo salio mal: {0} ", ex.Message);
+
+            }
+
+        }
+
+        private static void MiMetodo(){
+            throw miExcepcion;
+         }
+
+       
+    }
+   /*ublic class excepciones : ApplicationException
+    {
+        private Double num1, num2, num3;
+      
+
+
+        public override string Message {
+        }
+
+
+    }*/
+}
