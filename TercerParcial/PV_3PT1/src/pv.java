a129aceb53ce43cf194184af504ed46788c0b408
jaimeSanchezL1997 <48300839+jaimeSanchezL1997@users.noreply.github.com>
2019-06-25 18:49:09 -0500

    Add files via upload

--- TercerParcial/PV_3PT1/src/pv.java
+++ TercerParcial/PV_3PT1/src/pv.java
@@ -21,9 +21,31 @@ public class pv {
         System.out.println("cuantas rotaciones quiere?");
     rotacion = intput.nextInt();
 
-    for(int x = 0 ;x<arreglo.length;x++){
+  /*  for(int x = 0 ;x<arreglo.length;x++){
+        arreglo[numero-rotacion]=arreglo[x];
+
         arreglo[x]=arreglo[x+rotacion];
         System.out.println(arreglo[x]);
+    }*/
+
+            for(int x=1;x<=rotacion;x++) {
+
+                int temp = arreglo[0];
+                for(int i=0;i<arreglo.length-1;i++){
+                    arreglo[i] = arreglo[i+1];
+                }
+                arreglo[arreglo.length-1] = temp;
+            }
+
+
+
+
+
+            for(int x = 0;x<arreglo.length;x++){
+                System.out.print(arreglo[x]);
+            }
+            System.out.println();
+
     }
 
 
@@ -32,4 +54,4 @@ public class pv {
 
 
 
-}
+
